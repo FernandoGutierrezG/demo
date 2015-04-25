8925065db599c8c64b4f090d813ddfd2bb0a4a2c
Fernando <jesus.gutierrez@uabc.edu.mx>
2015-04-25 14:21:05 -0700

    commit 4

--- main.c
+++ main.c
@@ -3,17 +3,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 
-
+void imprimir(int item);
 
 void main()
 {
+	
 	int x, y;
 	printf("Hello World");
 	prinf("Dame un numero:");
 	scanf("%d",&x);
 	y=x*3;
-	prinf("El numero por tres es %d:",y);
+	imprimir(y);
 	getch();
+}
+
+
+void imprimir(int item)
+{
+	printf("La values es:%d",item);
 }
